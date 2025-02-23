b832fd5c7436d39219dcdce596f33a83b9d9b68a
Lars Ratovo <lars.ratovo@gmail.com>
2025-02-23 17:54:33 +0300

    Application initialization

---
+++ src/main/java/com/ratovo/KitapoMbola/KitapoMbolaApplication.java
@@ -0,0 +1,13 @@
+package com.ratovo.KitapoMbola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitapoMbolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitapoMbolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ratovo/KitapoMbola/KitapoMbolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratovo.KitapoMbola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitapoMbolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
